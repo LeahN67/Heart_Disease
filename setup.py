2872d14007d95cdbc4f0d586b41de26da870ee8c
Leah <leah@utu.io>
2023-01-16 21:26:03 +0300

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The objective is to leverage the powerful features of DevOps like CI/CD, automation, workflows and apply them to our data science projects & experiments with MLOps. ',
+    author='LeahN67',
+    license='MIT',
+)
